4f97de06eeb95d3282ac685a06a17e51d0cac1c0
Endy0611 <endyong18@gmail.com>
2026-03-16 17:04:26 +0700

    at new end point about getAllBook

--- src/main/java/com/example/batis_demo/controller/BookController.java
+++ src/main/java/com/example/batis_demo/controller/BookController.java
@@ -1,9 +1,11 @@
 package com.example.batis_demo.controller;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1/book")
 public class BookController {
 
 }
