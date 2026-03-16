9be70aeee55ede6082bd3165b3266af4f425e244
Endy0611 <endyong18@gmail.com>
2026-03-16 13:11:32 +0700

    push default project "

---
+++ src/main/java/com/example/batis_demo/BatisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.batis_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatisDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/batis_demo/BatisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batis_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
